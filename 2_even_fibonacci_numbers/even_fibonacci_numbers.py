09421f957a17876eeea3933891de0a2f70c8a6f4
Jack Dewhirst <jackdewhirst@gmail.com>
2021-01-19 16:10:31 +0000

    problem 2, faster soltuion

--- 2_even_fibonacci_numbers/even_fibonacci_numbers.py
+++ 2_even_fibonacci_numbers/even_fibonacci_numbers.py
@@ -1,15 +1,30 @@
 #By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.
 
+# evenFibNums = []
+# sumOfEvenFibNums = 2
+# prev1 = 1
+# prev2 = 2
+# while prev2 < 4000000:
+    # nextVal = prev1 + prev2
+    # #print(prev1,prev2,nextVal)
+    # if nextVal%2 == 0:
+        # sumOfEvenFibNums += nextVal
+    # prev1 = prev2
+    # prev2 = nextVal
+    
+# print(sumOfEvenFibNums)
+
+# better method. Every third fib number is even.
+
 evenFibNums = []
-sumOfEvenFibNums = 2
+sumOfEvenFibNums = 0
 prev1 = 1
-prev2 = 2
+prev2 = 1
+nextVal = prev1 + prev2
 while prev2 < 4000000:
+    sumOfEvenFibNums += nextVal
+    prev1 = prev2 + nextVal
+    prev2 = nextVal + prev1
     nextVal = prev1 + prev2
-    #print(prev1,prev2,nextVal)
-    if nextVal%2 == 0:
-        sumOfEvenFibNums += nextVal
-    prev1 = prev2
-    prev2 = nextVal
     
 print(sumOfEvenFibNums)
