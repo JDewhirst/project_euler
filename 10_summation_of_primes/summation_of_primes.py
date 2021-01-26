22b41c61985ee534cb1362d892fa9c27b55ebbe4
Jack Dewhirst <jackdewhirst@gmail.com>
2021-01-26 12:41:28 +0000

    problem 10, much faster sieve of Erastosthenes solution

--- 10_summation_of_primes/summation_of_primes.py
+++ 10_summation_of_primes/summation_of_primes.py
@@ -5,6 +5,8 @@ Find the sum of all the primes below two million.
 '''
 
 # isPrime fn from problem 7
+# this if very brute force, has complexity of sqrt(n)
+'''
 def isPrime(n):
     if (n <= 1): return False
     if (n == 2): return True
@@ -29,3 +31,24 @@ while i<N:
     i += 4
         
 print(primes_sum)
+'''
+## This is a soln using the sieve of Eratosthenes
+from math import floor
+N = 2000000
+cross_limit = floor(2000000**0.5)
+sieve = [False for a in range(N)]
+
+for i in range(4,N,2):
+    #print(i)
+    sieve[i] = True
+    
+for i in range(3,cross_limit,2):
+    if not sieve[i]: 
+        for j in range(i**2,N,2*i):
+            sieve[j] = True
+            
+prime_sum = 0
+for i in range(2,N):
+    if not sieve[i]: prime_sum += i
+
+print(prime_sum)
